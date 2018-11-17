f362dc227ece419f7b85b38242002901b6f21a90
Matheus Almeida <mat.almeida@live.com>
2018-11-17 18:57:12 -0300

    fix: removing args debug

--- client.py
+++ client.py
@@ -22,7 +22,6 @@ import time
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Naval Fate 2.0')
-    print(arguments)
 
     message = arguments['-m']
     repetitions = arguments['-n']

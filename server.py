0689c1e0bba5071a4e2d6c278b66406d29103c11
Matheus Almeida <mat.almeida@live.com>
2018-11-17 18:57:00 -0300

    feat:  adding better print feedback

--- server.py
+++ server.py
@@ -9,8 +9,16 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((HOST, PORT))
 
 while True:
-    msg = s.recv(30).decode('utf-8')
-    if msg == 'ping':
-        print("pong")
+    received = s.recvfrom(4096)
+    rMsg = received[0].decode('utf-8')
+    rHost = received[1][0]
+    rPort = received[1][1]
+
+    if rMsg == 'ping':
+        print(
+            "Received: '{0}' from {1}:{2} - PONG!".format(rMsg, rHost, rPort))
+    else:
+        print(
+            "Received: '{0}' from {1}:{2}".format(rMsg, rHost, rPort))
 
     time.sleep(1)
